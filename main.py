eeea52e27c2afeb706ff623ae7edf98169d9a929
DakshKikan <daksh.kikan@ais.amity.edu>
2024-02-04 15:51:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import perceval as pcvl
 import auto_grader
 import math
-pcvl.catalog["postprocessed ccz"].build_processor
+pcvl.catalog["postprocessed     ccz"].build_processor
 def get_CCZ() -> pcvl.Processor:
-    return pcvl.catalog["postprocessed_ccz"].build_processor()
-auto_grader.n_photons=1203103123012931
-auto_grader.n_modes=10221525125
+    return pcvl.catalog["postprocessed ccz"].build_processor()
+auto_grader.n_photons=0
+auto_grader.n_modes=0
 
 
 states={pcvl.BasicState('|1,0,1,0,1,0>'): '000',
@@ -16,11 +16,8 @@ states={pcvl.BasicState('|1,0,1,0,1,0>'): '000',
         pcvl.BasicState('|0,1,1,0,0,1>'): '101',
         pcvl.BasicState('|0,1,0,1,1,0>'): '110',
         pcvl.BasicState('|0,1,0,1,0,1>'): '111'}
-
-
 target={"000": "000", "001": "001", "010": "010", "011": "011",
               "100": "100", "101": "101", "110": "111", "111": "110"}
-
 
 ca = pcvl.algorithm.Analyzer(pcvl.catalog["postprocessed ccz"].build_processor(), states)
 
@@ -29,3 +26,4 @@ ca.compute(target)
 print(ca.performance)
 
 print(ca.fidelity)
+print(auto_grader.n_photons,auto_grader.n_modes)
